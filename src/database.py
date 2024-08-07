962c690f893a406777f3ec36aeeb9443380c0aed
Art Rookijakers <rookart253@gmail.com>
2024-08-07 08:50:24 +0800

    .

--- src/database.py
+++ src/database.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import random
 import os
-
+import numpy as np
 import time
 from datetime import datetime
 
 
 # File path for the CSV file
 csv_file = 'src/database.csv'
+temp_file = 'src/tempanalysis.csv'
+ec_file = 'src/ecanalysis.csv'
 # Function to log data into the CSV file
 def log_data(temp, ecval, light, phval, humidity):
     # Get the current timestamp
@@ -36,14 +38,39 @@ def log_data(temp, ecval, light, phval, humidity):
         # Create the CSV file with the header
         df.to_csv(csv_file, mode='w', index=False, header=True)
 
+# Function to read the data from the CSV file into a DataFramew
+def analyze_data(df):
+    analysis = {}
+    columns = ['EC Level', 'Temperature', 'Humidity', 'pH Level', 'Light Level']
+    
+    for col in columns:
+        data = df[col].values
+        analysis[col] = {
+            'mean': np.mean(data),
+            'median': np.median(data),
+            'std_dev': np.std(data)
+        }
+    
+    return analysis
+
+
+
 # Function to read the data from the CSV file into a DataFrame
 def read_data():
-    # Check if the CSV file exists and is not empty
     if os.path.exists(csv_file) and os.path.getsize(csv_file) > 0:
-        # Read the CSV file into a DataFrame
         df = pd.read_csv(csv_file)
         return df
     else:
-        # Return an empty DataFrame if the file does not exist or is empty
         return pd.DataFrame(columns=['timestamp', 'EC Level', 'Temperature', 'Humidity', 'pH Level', 'Light Level'])
+
+def historical_data(df):
+    
+    
+def main():
+    df=read_data()
+
+
+
+if __name__ == '__main__':
+    main()
 
